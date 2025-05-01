b3b59e5825a6d9f38c02f5465d092a8cfa6fdd7c
Learnedprawn <vineshparalkar@gmail.com>
2025-05-02 00:11:55 +0530

    dummy test

---
+++ contracts/test/LocationVaultTest.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity 0.8.24;
+
+import {Test, console} from "forge-std/Test.sol";
+import {LocationVault} from "../src/LocationVault.sol";
+
+contract LocationVaultTest is Test {
+  LocationVault vault;
+  uint256 constant AMOUNT = 1 ether;
+
+  function setUp() public {
+    vault = new LocationVault(AMOUNT);
+  }
+
+  function test__dummy() pure public {
+    require(1 == 1);
+  }
+}
