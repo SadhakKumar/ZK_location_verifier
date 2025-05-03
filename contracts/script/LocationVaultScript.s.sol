e84629670399777ed04af5d74cec194125f662c5
Learnedprawn <vineshparalkar@gmail.com>
2025-05-04 00:35:25 +0530

    zk contract added and script

--- contracts/script/LocationVaultScript.s.sol
+++ contracts/script/LocationVaultScript.s.sol
@@ -3,14 +3,16 @@ pragma solidity 0.8.24;
 
 import {Script} from "forge-std/Script.sol";
 import {LocationVault} from "../src/LocationVault.sol";
+import {Groth16Verifier} from "../src/ZkContract.sol";
 
 contract LocationVaultScript is Script {
 
   uint256 public constant AMOUNT = 1 ether;
-  function run() external returns (LocationVault) {
+  function run() external returns (LocationVault, Groth16Verifier) {
     vm.startBroadcast();
-    LocationVault vault = new LocationVault(AMOUNT);
+    Groth16Verifier zkContract = new Groth16Verifier();
+    LocationVault vault = new LocationVault(AMOUNT, address(zkContract));
     vm.stopBroadcast();
-    return vault;
+    return (vault, zkContract);
   }
 }
--- contracts/src/LocationVault.sol
+++ contracts/src/LocationVault.sol
@@ -6,20 +6,30 @@ import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol
 contract LocationVault is ReentrancyGuard {
     error LocationVault__StakedValueInsufficient();
     error LocationVault__InsufficientBalance();
+    error LocationVault__NotZkContract();
 
     event Staked(address staker, uint256 amount);
 
-    modifier withinLocationAndTimePeriod() {
+    // modifier withinLocationAndTimePeriod() {
+    //     _;
+    // }
+
+    modifier onlyZkContract() {
+        if (msg.sender != zkContract) {
+            revert LocationVault__NotZkContract();
+        }
         _;
     }
 
+    address public zkContract;
     bool public locked;
     uint256 public amount;
     mapping(address immigrant => uint256 amount) balances;
 
-    constructor(uint256 _amount) {
+    constructor(uint256 _amount, address _zkContract) {
         amount = _amount;
         locked = false;
+        zkContract = _zkContract;
     }
 
     function stake() public payable {
@@ -31,7 +41,7 @@ contract LocationVault is ReentrancyGuard {
         }
     }
 
-    function withdraw(uint256 _amount) public nonReentrant withinLocationAndTimePeriod {
+    function withdraw(uint256 _amount) public nonReentrant {
         if (balances[msg.sender] < _amount) {
             revert LocationVault__InsufficientBalance();
         } else {
